f697ffca3a8b533a8b1d3a5f07ddc7d8a1e06768
Weblymit <weblymit@gmail.com>
2023-03-14 11:04:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include('partials/_header.php')
+?>
+<main class="pt-20">
+  <p class="text-xl">
+    Bienvenue en 2023. Dans ce cours nous apprendre le traitement des formulaire en PHP et les superglobales 😊 ! Are your ready ?
+  </p>
+</main>
+<!-- footer -->
+<?php
+include('partials/_footer.php')
+?>
---
+++ partials/_header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Formulaire</title>
+</head>
+
+<body class="p-20">
+  <h1 class="font-black text-3xl text-center">Cours n°2 PHP</h1>
+  <nav class="text-center text-blue-500 pt-10 space-x-6">
+    <a href="index.php">Accueil</a>
+    <a href="index.php">Get</a>
+    <a href="index.php">Post</a>
+    <a href="index.php">Validation formulaire</a>
+    <a href="index.php">Upload fichier</a>
+  </nav>
